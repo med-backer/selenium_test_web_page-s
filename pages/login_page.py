48111bb543d39586c6d6a07cc5064f1f815620bb
med.backer <med.backer@yandex.kz>
2021-02-21 21:38:43 +0600

    add test for guest can see authorization form

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+from pages.main_page import MainPage
+class LoginPage(MainPage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url.find("login")>-1, "Its page isnt login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)>0,"Login form not here"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)>0,"Registor form not here"
