572c840a856ee20251014f7de14d2e58a109213f
med.backer <med.backer@yandex.kz>
2021-02-23 22:04:10 +0600

    add pytest.parametrize and mark.xfail in parametrize

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class LoginPageLocators():
     LOGIN_FORM=(By.CSS_SELECTOR,"form#login_form")
     REGISTER_FORM=(By.CSS_SELECTOR,"form#register_form")
     
+class ProductPageLocators():
+    BTN_ADD_TO_BASKET=(By.CSS_SELECTOR,"button.btn-add-to-basket")
+    PRODUCT_NAME=(By.CSS_SELECTOR,".product_page h1")
+    PRODUCT_NAME_IN_MESSAGES_AFTER_ADD=(By.CSS_SELECTOR,"div#messages .alert:nth-child(1) strong")
+    PRODUCT_PRICE=(By.CSS_SELECTOR,"table tr:nth-child(4) td")
+    BASKET_PRICE=(By.CSS_SELECTOR,".alert-info .alertinner strong")
+    
---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def press_to_add_basket(self):
+        btn_add=self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        btn_add.click()
+    def should_be_product_in_basket(self):
+        product_name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_in_messages_after_add=self.browser.find_element(
+                    *ProductPageLocators.PRODUCT_NAME_IN_MESSAGES_AFTER_ADD).text
+        assert product_name == product_name_in_messages_after_add,f"Product\
+                {product_name} isn't Product {product_name_in_messages_after_add}"
+    def should_be_price_in_basket_equal_price_product(self):
+        product_price=float(self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.split()[0].replace(",",".").replace("\xa3",""))
+        basket_price=float(self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text.split()[0].replace(",",".").replace("\xa3",""))
+        assert product_price == basket_price,"Price product {product_price} isn't equal basket price {basket_price}"
